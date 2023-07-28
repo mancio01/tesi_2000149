ec851e9517a29fad80d80e2935e8cc280ffe957a
mancio01 <96566345+mancio01@users.noreply.github.com>
2023-07-28 16:05:11 +0200

    Update turing_test.py

--- turing_test.py
+++ turing_test.py
@@ -2,8 +2,12 @@ import os
 import cv2
 import json
 
+#percorso file con cartella filtering in tesi_2000149 
 path = os.path.dirname(os.path.abspath(__file__)) + r"\filtering\train\\"
+
+#estrazione dei nomi delle cartelle presenti in filtering
 folders = [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
+
 ratings = {}
 for i in folders:
     imm = cv2.imread(path + i + r"\DisplayImage.png")
@@ -15,6 +19,8 @@ for i in folders:
         val = True
     else:
         val = False        
-    ratings[i] = val
+    ratings[i] = val  # nome del file (key), voto (value)
+
+#salvataggio JSON Array in file json
 with open(os.path.dirname(os.path.abspath(__file__))+r"\ratings.json", 'w') as json_file:
     json.dump(ratings, json_file)
